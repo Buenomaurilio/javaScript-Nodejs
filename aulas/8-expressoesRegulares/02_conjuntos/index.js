53971ac1d9bdc06620f18c42d9341238fb440f53
Buenomaurilio <mauriliodsb@gmail.com>
2020-05-13 13:35:19 -0300

    + Regex ...

--- aulas/8-expressoesRegulares/02_conjuntos/index.js
+++ aulas/8-expressoesRegulares/02_conjuntos/index.js
@@ -7,5 +7,11 @@ console.log(reg1.test("Temos o número 60"))
 
 const reg2 = /[0-9]/
 
-console.log(reg1.test("Temos o número 65448484884848484"))
+console.log(reg2.test("Temos o número 65448484884848484"))
 console.log(reg1.test("Temos o número"))
+const reg3 = /[0-3]/
+
+console.log(reg2.test('kjsdnafiuandfnad152ckjdsakcb485'))
+
+
+console.log(reg3.test('udufibndskjn9845UIDSUBFAS'))
